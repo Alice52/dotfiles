c8d234873b23c72434b1b0846efc82d9476433e5
Alice52 <zzhang_xz@163.com>
2022-07-26 00:08:28 +0800

    replace docker source to aliyun

--- ubuntu/docker-install.sh
+++ ubuntu/docker-install.sh
@@ -30,8 +30,12 @@ sudo apt-get update
 sudo apt-get install -y gcc
 
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+
 
 sudo apt-get -y update
 
