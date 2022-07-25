4cdde31c989d34f9cbc9160c3f8ff7e2ef246082
Alice52 <42330329+Alice52@users.noreply.github.com>
2022-07-25 22:28:08 +0800

    add autosuggestions plugin

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ echo "Setting up the Spaceship theme."
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting --depth=1
 git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z --depth=1
 
