2497935074c8d8e505eaa5220b815654582c3ef0
Alice52 <zzhang_xz@163.com>
2022-07-25 23:26:24 +0800

    ubuntu: add setup with docker & zsh

--- ubuntu/docker-install.sh
+++ ubuntu/docker-install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
 sudo apt-get -y update
+
 sudo apt-get -y install docker-ce
 
 sudo systemctl enable docker
--- ubuntu/zsh-install.sh
+++ ubuntu/zsh-install.sh
@@ -2,6 +2,4 @@
 
 sudo apt-get install -y zsh
 
-sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-source ~/.zshrc
+zsh --version
