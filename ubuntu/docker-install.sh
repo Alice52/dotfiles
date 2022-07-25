9edde372dbd091d6722065a3bc4ceea8a4259bef
Alice52 <42330329+Alice52@users.noreply.github.com>
2022-07-25 23:45:45 +0800

    update to ubuntu source

--- ubuntu/docker-install.sh
+++ ubuntu/docker-install.sh
@@ -4,29 +4,26 @@ sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo tee /etc/apt/sources.list <<-'EOF'
 # add this code
   #添加阿里源
-  deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
-  deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
-  deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
-  deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
-  deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
-  #中科大源
-  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
-  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
-  #163源
-  deb http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse
-  deb http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse
-  deb http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse
-  deb http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse
-  deb http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse
-  #清华源
-  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
-  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+  deb http://mirrors.aliyun.com/ubuntu/ focal main universe multiverse restricted
+  deb http://mirrors.aliyun.com/ubuntu/ focal-security main universe multiverse restricted
+  deb http://mirrors.aliyun.com/ubuntu/ focal-updates main universe multiverse restricted
+  deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main universe multiverse restricted
+  deb http://mirrors.aliyun.com/ubuntu/ focal-backports main universe multiverse restricted
+  deb-src http://mirrors.aliyun.com/ubuntu/ focal main universe multiverse restricted
+  deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main universe multiverse restricted
+  deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main universe multiverse restricted
+  deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main universe multiverse restricted
+  deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main universe multiverse restricted
+
+  #添加清华源
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+  # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+  # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+  # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+  # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse multiverse
 EOF
 
 sudo apt-get update
