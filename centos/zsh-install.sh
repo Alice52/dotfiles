587f8c6dd44c375d212e1ae934eedf1e409b8d44
Alice52 <42330329+Alice52@users.noreply.github.com>
2022-07-25 22:58:06 +0800

    replace github source to gitee

--- centos/zsh-install.sh
+++ centos/zsh-install.sh
@@ -2,7 +2,7 @@
 
 sudo yum install -y git make ncurses-devel gcc autoconf man yodl
 sudo mkdir -p /tmp/zsh
-sudo git clone --depth 1 -b zsh-5.7.1 https://github.com/zsh-users/zsh.git /tmp/zsh
+sudo git clone --depth 1 -b zsh-5.7.1 https://gitee.com/github-mirror-zsh/zsh.git /tmp/zsh
 cd /tmp/zsh
 sudo ./Util/preconfig
 sudo ./configure
