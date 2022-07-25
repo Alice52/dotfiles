e4c9a296b8e27fedba4d71f4edfaf9d8acb52318
Alice52 <zzhang_xz@163.com>
2022-07-25 23:34:20 +0800

    replace ubuntu source

--- ubuntu/docker-install.sh
+++ ubuntu/docker-install.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo tee /etc/apt/sources.list <<-'EOF'
+# add this code
+  #添加阿里源
+  deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+  deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+  deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+  deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+  deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+  #中科大源
+  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
+  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+  #163源
+  deb http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse
+  deb http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse
+  deb http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse
+  deb http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse
+  deb http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse
+  #清华源
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+  deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+EOF
+
+sudo apt-get update
+sudo apt-get install -y gcc
+
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
