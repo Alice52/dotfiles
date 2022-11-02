ade051263b782db61554b0268b2f170966c861b2
alice52 <zzhang_xz@163.com>
2022-11-02 17:40:42 +0800

    github: add repo init & workspace

---
+++ github/repos.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# git clone git@github.com:Alice52/dotfiles.git
+git clone git@github.com:Alice52/go-tutorial.git
+git clone git@github.com:Alice52/algorithms.git
+git clone git@github.com:Alice52/tutorials-sample.git
+git clone git@github.com:Alice52/diagrams.git
+git clone git@github.com:Alice52/docker.git
+git clone git@github.com:Alice52/spring-5.2.x.git
+git clone git@github.com:Alice52/project.git
+git clone git@github.com:Alice52/uid-generator.git
+git clone git@github.com:Alice52/grpc-tutorials.git
+
+git clone git@github.com:Alice52/java-ocean.git
+cd java-ocean && git submodule init && git submodule update --remote && cd ..
+
+git clone git@github.com:Alice52/java-ocean.git
+cd java-ocean && git submodule init && git submodule update --remote && cd ..
+
+git clone git@github.com:Alice52/practice.git
+cd practice && git submodule init && git submodule update --remote && cd ..
+
+git clone git@github.com:Alice52/tools.git
+cd tools && git submodule init && git submodule update --remote && cd ..
+
+git clone git@github.com:Alice52/graphql.git
+cd graphql && git submodule init && git submodule update --remote && cd ..
+
+git clone git@github.com:Alice52/practice.git
+cd practice && git submodule init && git submodule update --remote && cd ..
+
+git clone git@github.com:Alice52/lite-starter.git
+cd lite-start && git submodule init && git submodule update --remote && cd ..
